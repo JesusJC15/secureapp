85742fe15f000b6ae0b3dec6b63bc33a8ea42823
JesusJC15 <jesus.jauregui-c@mail.escuelaing.edu.co>
2026-03-18 12:31:56 -0500

    getPort implemented

--- src/main/java/com/arep/secureapp/SecureappApplication.java
+++ src/main/java/com/arep/secureapp/SecureappApplication.java
@@ -9,9 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecureappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(SecureappApplication.class); 
-        app.setDefaultProperties(Collections.singletonMap("server.port", 5000));
-        app.run(args); 
+		SpringApplication app = new SpringApplication(SecureappApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+		app.run(args);
+	}
+
+	static int getPort() {
+		if (System.getenv("PORT") != null) {
+			return Integer.parseInt(System.getenv("PORT"));
+		}
+		return 5000; // returns default port if heroku-port isn't set (i.e. on localhost)
 	}
 
 }
